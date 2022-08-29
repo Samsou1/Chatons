0c52907b9a34cdeb20b2cf7c1bdee4dd80cf46d1
Pierre-François Salmand <71193709+Pefington@users.noreply.github.com>
2022-08-29 20:56:22 +0700

    Correct price seed (#8)

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ until Item.count == 20
   Item.create!(
     title: Faker::Creature::Cat.unique.name,
     description: Faker::Artist.unique.name,
-    price: Faker::Number.between(from: 500, to: 10_000),
+    price: Faker::Number.decimal(l_digits: 2, r_digits: 2),
     image_url: 'https://source.unsplash.com/random/?kitten'
   )
 end
