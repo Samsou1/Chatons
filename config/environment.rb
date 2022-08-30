4985b0dd4891f66659eabda29b3da5502eb94db4
PierreAND <andrieu@gmail.com>
2022-08-30 16:30:32 +0200

    rubocop rectification

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  user_name: "apikey",
+  user_name: 'apikey',
   password: Rails.application.credentials.sendgrid[:secret_key],
   domain: 'https://chaton-shop.herokuapp.com/',
   address: 'smtp.sendgrid.net',
