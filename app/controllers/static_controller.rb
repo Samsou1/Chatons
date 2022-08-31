3c1127ae9ff216f454b8dd57a07942d9f3328eb0
Samsou1 <sam.moreau0@gmail.com>
2022-08-31 19:34:00 +0200

    profile with orders

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -6,10 +6,17 @@ class StaticController < ApplicationController
   def profile
     @user = current_user
     @orders = Order.where(user_id: current_user.id)
-    @oderitems = []
+    @list_of_order_items = []
     @orders.each do |order|
-      orderitems = Odertitems.where(order_id: order.id)
-      @orderitems.push(orderitems)
+      @list_of_order_items.push(Orderitem.where(order_id: order.id))
+    end
+    @items = []
+    @list_of_order_items.each_with_index do |order_items, _index|
+      ary = []
+      order_items.each do |_order_item|
+        ary.push(Item.find_by(id: _order_item.item_id))
+      end
+      @items.push(ary)
     end
   end
 end
