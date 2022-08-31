3061caf1ce5e65e67eb3850ef562e2c3a957b507
PierreAND <andrieu@gmail.com>
2022-08-31 11:40:27 +0200

    add both view for email order and add colum information in User

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,9 @@
 class OrdersController < ApplicationController
+    def show
+      @order = Order.find(params[:id])
+    end
+
+    def new
+      @order = Order.new
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_164613) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_31_093626) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,10 +24,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_164613) do
   end
 
   create_table "carts", force: :cascade do |t|
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
   create_table "items", force: :cascade do |t|
@@ -62,6 +60,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_164613) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "zip_code"
+    t.text "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
