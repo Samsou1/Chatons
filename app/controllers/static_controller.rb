6074f68d3d05371bfefbdce807a32496095e8655
Samsou1 <sam.moreau0@gmail.com>
2022-08-31 14:46:31 +0200

    profile routing controller

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,2 +1,10 @@
 class StaticController < ApplicationController
+  before_action :authenticate_user!, only: %i[profile]
+  def contact; end
+  def about; end
+
+  def profile
+    @user = current_user
+    puts @user
+  end
 end
