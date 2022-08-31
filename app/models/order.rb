04210ba14bfbb7038f6456a4c77295d7d0f2bb2c
Sam <104089857+Samsou1@users.noreply.github.com>
2022-08-31 21:02:25 +0200

    profile with orders (#35)

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,9 @@
 class Order < ApplicationRecord
-  belongs_to :user
-
   has_many :orderitems
   has_many :items, through: :orderitems
   belongs_to :user
 
-  after_create :order_send
+  # after_create :order_send
 
   def order_send
     UserMailer.email_order(self).deliver_now
