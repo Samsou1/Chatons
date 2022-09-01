6043479d3fd69d8d7aa2b765e4435f5cb829aa75
PierreAND <108337086+PierreAND@users.noreply.github.com>
2022-09-01 11:21:26 +0200

    Read me (#39)

--- app/controllers/orders_controller.rb
+++
@@ -1,9 +0,0 @@
-class OrdersController < ApplicationController
-    def show
-      @order = Order.find(params[:id])
-    end
-
-    def new
-      @order = Order.new
-    end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_08_31_093626) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,8 +53,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_31_093626) do
 
   create_table "carts", force: :cascade do |t|
     t.bigint "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
     t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
