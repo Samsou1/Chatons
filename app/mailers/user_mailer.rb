922522a7499be5b9082f403652a440e0c3687b9f
Pierre-François Salmand <71193709+Pefington@users.noreply.github.com>
2022-09-02 05:01:28 +0700

    Mailer (#58)

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
     @url = 'https://kittenfest.herokuapp.com/'
-    mail(to: @user.email, subject: 'Welcome to Chaton-Shop')
+    mail(to: @user.email, subject: 'Welcome to Kittenfest')
   end
 
   def email_order(order)
@@ -12,6 +12,6 @@ class UserMailer < ApplicationMailer
     @user = @order.user
     @url = 'https://kittenfest.herokuapp.com/'
     @items = @order.items
-    mail(to: @user.email, subject: 'Order on CatShop')
+    mail(to: @user.email, subject: 'Order on Kittenfest')
   end
 end
