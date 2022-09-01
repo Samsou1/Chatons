05323965adf04f120391a1ce1952d4eb5b6d0a40
Pierre-François Salmand <maabaa@gmail.com>
2022-09-02 03:43:07 +0700

    Fix non defered JS

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,2 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-
-import Alpine from "alpinejs"
-window.Alpine = Alpine
-
-document.addEventListener("DOMContentLoaded", function(event) {
-  window.Alpine.start();
-});
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,4 +2,3 @@
 
 pin 'application', preload: true
 pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
-pin 'alpinejs', to: 'https://ga.jspm.io/npm:alpinejs@3.10.3/dist/module.esm.js'
