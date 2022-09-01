e8f3b4e09ad519e43c313b44fa75d9341a24471a
pandaka87 <noellie.peuch2@gmail.com>
2022-09-01 13:01:52 +0200

    Change routes for orders

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,14 @@ Rails.application.routes.draw do
     resources :images, only: [:create]
   end
   resources :carts
-  resources :orders, only: %i[create index show]
+  resources :orders, only: %i[create]
   resources :cart_items, only: %i[create destroy]
   resources :order_items, only: %i[create destroy]
   devise_for :users
   resources :users, only: [:show]
   resources :users, only: [:show] do
     resources :profile_pictures, only: [:create]
+    resources :orders, only: %i[show index]
   end
   post 'checkouts/create' => 'checkouts#create', as: 'checkouts_create'
   resources :webhooks, only: [:create]
