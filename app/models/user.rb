3ffe446a1a0c882a9b27cf8a1e1789e2ad414873
Gregory <gregory.laigneau@protonmail.com>
2022-09-01 15:13:47 +0200

    Add user role

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,16 @@ class User < ApplicationRecord
   has_one :cart
   has_one_attached :profile_picture
 
+  after_initialize :set_default_role, if: :new_record?
   after_create :welcome_send
 
   def welcome_send
     UserMailer.welcome_email(self).deliver_now
+  end
+
+  enum role: { user: 0, admin: 1 }
+
+  def set_default_role
+    self.role ||= :user
   end
 end
