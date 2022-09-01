9a7604255208cbfee10ed52740e507a66b396731
Sam <104089857+Samsou1@users.noreply.github.com>
2022-09-01 15:30:40 +0200

    Cart with same items (#41)

--- app/models/cartitem.rb
+++ app/models/cartitem.rb
@@ -1,5 +1,5 @@
 class Cartitem < ApplicationRecord
-  validates :cart_id, uniqueness: { scope: :item_id }
+  # validates :cart_id, uniqueness: { scope: :item_id }
   belongs_to :item
   belongs_to :cart
 end
