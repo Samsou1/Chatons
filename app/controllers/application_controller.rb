9e8540c0730912d5fd89410323948c9ebe66ab73
Samsou1 <sam.moreau0@gmail.com>
2022-09-01 22:20:41 +0200

    start flash + get rid of cart buttons + delete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  add_flash_types :danger, :info, :warning, :success, :messages
+  
   def clear_cart
     @cart = current_user.cart
     @cart.items = []
